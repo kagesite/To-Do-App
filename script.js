74261875beca3440b6d2f5eea31ddec04a8720c0
Kage Saluone <ksaluone17@gmail.com>
2024-09-19 22:28:15 -0600

    Started basic javascript function for creating a new list

---
+++ script.js
@@ -0,0 +1,8 @@
+function createNewList() {
+  const listsInput = document.getElementById('listsInput');
+  const listName = listsInput.value;
+
+  const newItem = document.createElement('li');
+  
+
+}
